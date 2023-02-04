1f4da9477d78eb8bc598f23e282eace4ae6b34fb
Marius Baden <marius.baden@student.kit.edu>
2023-02-04 16:16:55 -0500

    Implement basic functionality for CreditManager

---
+++ discord_front_end/credits/CreditManager.py
@@ -0,0 +1,66 @@
+import asyncio
+
+from discord_front_end.credits.HourlyCostThread import HourlyCostThread
+from discord_front_end.utils.db import MongoGateWay
+
+
+class CreditManager:
+    """
+    Provides to the credit balance of a guild and affords starting and stopping deduction of credits on hourly basis.
+
+    Acts as a Column Data Gateway for the credit column in the instances document database. Additionally, it
+    includes some business logic for using this credit information in order to do the deduction. This is a design
+    compromise chosen to accelerate implementation.
+    """
+
+    def __init__(self, database_gateway: MongoGateWay):
+        self.database_gateway = database_gateway
+
+        # Map of guild_id to credit deduction task, if there is a credit deduction task running for that guild
+        self.credit_deduction_task = {}
+
+    def get_credit_balance(self, guild_id: int) -> float:
+        """
+        Returns the credit balance of a guild
+        """
+        return self.database_gateway.find_instance_one(guild_id)["credits"]
+
+    def can_afford(self, guild_id: int, amount: float) -> bool:
+        """
+        Returns whether a guild can afford a certain amount of credits.
+
+        Convenience method for get_credit_balance(guild_id)
+        """
+        return self.get_credit_balance(guild_id) >= amount
+
+    def add_credits(self, guild_id: int, credits: float) -> None:
+        """
+        Adds credits to the credit balance of a guild
+        """
+        self.database_gateway.update_instance_one(guild_id, {"credits": self.get_credit_balance(guild_id) + credits})
+
+    def deduct_credits(self, guild_id: int, credits: float) -> None:
+        """
+        Deducts credits from the credit balance of a guild
+        """
+        self.database_gateway.update_instance_one(guild_id, {"credits": self.get_credit_balance(guild_id) - credits})
+
+    def start_deduction(self, guild_id: int, hourly_cost: float) -> None:
+        """
+        Starts a task that deducts credits from the credit balance of a guild on an hourly basis
+        """
+        if guild_id in self.credit_deduction_task:
+            raise Exception("There already is a credit deduction task already running for guild {}".format(guild_id))
+
+        self.credit_deduction_task[guild_id] = HourlyCostThread(guild_id, hourly_cost, self)
+        self.credit_deduction_task[guild_id].start()
+
+    def stop_deduction(self, guild_id: int) -> None:
+        """
+        Stops the task that deducts credits from the credit balance of a guild on an hourly basis
+        """
+        if guild_id not in self.credit_deduction_task:
+            raise Exception("There is no credit deduction task running for guild {}".format(guild_id))
+
+        self.credit_deduction_task[guild_id].stop()
+        del self.credit_deduction_task[guild_id]
