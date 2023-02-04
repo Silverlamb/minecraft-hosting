2607afbbb722c2c3a8fcecaeb44bfeac4019c2ab
Marius Baden <marius.baden@student.kit.edu>
2023-02-04 18:54:43 -0500

    Introduce CreditColumnDataGateway

--- discord_front_end/credits/CreditManager.py
+++ discord_front_end/credits/CreditManager.py
@@ -1,51 +1,24 @@
 import asyncio
 
 from discord_front_end.UserMessageResponder import UserMessageResponder
+from discord_front_end.credits.CreditColumnDataGateway import CreditColumnDataGateway
 from discord_front_end.credits.FixedTimeIntervalCostThread import FixedTimeIntervalCostThread
-from discord_front_end.utils.db import MongoGateWay
 
 
 class CreditManager:
     """
-    Provides to the credit balance of a guild and affords starting and stopping deduction of credits on hourly basis.
+    Affords starting and stopping deduction of credits on hourly basis.
 
-    Acts as a Column Data Gateway for the credit column in the instances document database. Additionally, it
-    includes some business logic for using this credit information in order to do the deduction. This is a design
+    It includes some business logic for using this credit information in order to do the deduction. This is a design
     compromise chosen to accelerate implementation.
     """
 
-    def __init__(self, database_gateway: MongoGateWay):
-        self.database_gateway = database_gateway
+    def __init__(self, credit_column_data_gateway: CreditColumnDataGateway):
+        self.credit_column_data_gateway = credit_column_data_gateway
 
         # Map of guild_id to credit deduction task, if there is a credit deduction task running for that guild
         self.credit_deduction_task = {}
         self.deduction_sleep_time = 3600
-
-    def get_credit_balance(self, guild_id: int) -> float:
-        """
-        Returns the credit balance of a guild
-        """
-        return self.database_gateway.find_instance_one(guild_id)["credits"]
-
-    def can_afford(self, guild_id: int, amount: float) -> bool:
-        """
-        Returns whether a guild can afford a certain amount of credits.
-
-        Convenience method for get_credit_balance(guild_id)
-        """
-        return self.get_credit_balance(guild_id) >= amount
-
-    def add_credits(self, guild_id: int, credits: float) -> None:
-        """
-        Adds credits to the credit balance of a guild
-        """
-        self.database_gateway.update_instance_one(guild_id, {"credits": self.get_credit_balance(guild_id) + credits})
-
-    def deduct_credits(self, guild_id: int, credits: float) -> None:
-        """
-        Deducts credits from the credit balance of a guild
-        """
-        self.database_gateway.update_instance_one(guild_id, {"credits": self.get_credit_balance(guild_id) - credits})
 
     def start_deduction(self, guild_id: int, hourly_cost: float, responder: UserMessageResponder) -> None:
         """
@@ -63,12 +36,12 @@ class CreditManager:
         if guild_id in self.credit_deduction_task:
             raise Exception("There already is a credit deduction task already running for guild {}".format(guild_id))
 
-        if not self.can_afford(guild_id, hourly_cost):
-            balance = self.get_credit_balance(guild_id)
+        if not self.credit_column_data_gateway.can_afford(guild_id, hourly_cost):
+            balance = self.credit_column_data_gateway.get_credit_balance(guild_id)
             raise Exception("Your credits ({}) are not sufficient to pay the first interval.".format(balance))
 
         self.credit_deduction_task[guild_id] = FixedTimeIntervalCostThread(guild_id, hourly_cost,
-                                                                           self.deduction_sleep_time, self)
+                                                                           self.deduction_sleep_time, self, responder)
         self.credit_deduction_task[guild_id].start()
 
     def stop_deduction(self, guild_id: int) -> None:
@@ -86,4 +59,4 @@ class CreditManager:
         not_used_seconds = self.deduction_sleep_time - (uptime % self.deduction_sleep_time)
         not_used_cost = (not_used_seconds / self.deduction_sleep_time) * \
                           self.credit_deduction_task[guild_id].get_hourly_cost()
-        self.add_credits(guild_id, not_used_cost)
+        self.credit_column_data_gateway.add_credits(guild_id, not_used_cost)
