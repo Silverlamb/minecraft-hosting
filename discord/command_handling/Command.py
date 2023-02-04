81ea2cf69d9456e15c6dc2433748197864076ea7
Marius Baden <marius.baden@student.kit.edu>
2023-02-04 02:43:17 -0500

    Add name field to command interface

--- discord/command_handling/Command.py
+++ discord/command_handling/Command.py
@@ -6,13 +6,34 @@ Abstract class that represents a general command.
 A command must be executable with through its 'execute()' method.
 Every concrete command class has a constructor that takes the commands arguments as a string array and parses them.
 """
+
+
 class Command:
+
+    """
+    Abstract constructor for a command. Should not be called from outside a constructor of a concrete command class.
+
+    Specifies the name of the command.
+    """
+    def __init__(self, name: str):
+        self.name = name
 
     """
     Executes the command.
 
     Usually, one command object will only be executed once.
     """
+
     @abstractmethod
     def execute(self):
         pass
+
+    """
+    Returns the name of this command.
+    
+    Should usually be a single lowercase word.
+    """
+
+    def get_name(self) -> str:
+        return self.name
+
--- discord/command_handling/CommandParser.py
+++ discord/command_handling/CommandParser.py
@@ -1,14 +1,21 @@
-import Command
+from command_handling.Command import Command
 
 """
 For a given command string, creates the appropriate command objects and gives them the necessary arguments.
 """
 class CommandParser:
     def __init__(self):
+        # List of the commands that this parser can parse
+        self.command_list = []
         pass
 
     """
     Takes a command string and creates 
     """
     def parse_command(self, command_string: str) -> Command:
-        pass
+        string_elements = command_string.split(" ")
+        for command in self.command_list:
+            if command.get_name() == string_elements[0]:
+                return command(string_elements[1:])
+
+
